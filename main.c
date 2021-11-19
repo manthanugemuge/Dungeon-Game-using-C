c6751886102f1a0b677d57ea6e3522084fa59dc2
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2021-11-20 02:49:31 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <string.h>
 
+int main()
+{
+  char choice[10];
+  while (strcmp(choice, "quit") != 0)
+  {
+    printf("You are stuck in a dungeon cell. Which direction do you want to look?\n");
+    scanf("%s", choice);
+    if (strcmp(choice, "north") == 0)
+    {
+      printf("You see a window with bars on it.\n");
+    }
+    if (strcmp(choice, "east") == 0)
+    {
+      printf("You see stone wall.\n");
+    }
+    if (strcmp(choice, "west") == 0)
+    {
+      printf("You see a monster.\n");
+    }
+    if (strcmp(choice, "south") == 0)
+    {
+      printf("You see an open door. You escape through the door and win the game!\n");
+      break;
+    }
+  }
+  printf("Bye!");
+  return 0;
+}
